52910af9b0c3f42c2d02d79e78b1d49e02046d43
Bruno Borges Russian <bruno.russian.i@gmail.com>
2022-11-26 15:48:44 -0500

    Add LoginHandler

---
+++ Omnicorp/LoginHandler.cs
@@ -0,0 +1,86 @@
+﻿using ClassLibrary;
+using Omnicorp.Admin;
+using Omnicorp.Buyer;
+using Omnicorp.Exceptions;
+using Omnicorp.Planner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows;
+
+namespace Omnicorp
+{
+    public class LoginHandler
+    {
+
+        public void TryLogin(string usernameInput, string passwordInput)
+        {
+            
+            MyQuery myQuery = new MyQuery();
+
+            string query = $"SELECT role, password FROM users WHERE username = '{usernameInput}';";
+            MySqlDataReader rdr = myQuery.DataReader(query);
+
+            if (rdr.Read())                                         // If the row exists in Db
+            {
+                string roleDb = rdr.GetString(0).ToString();        // store role from users table
+                string passwordDb = rdr.GetString(1).ToString();    // store password form users table
+
+
+                // Validate if user and password entered in textbox match query
+                VerifyPassword(passwordDb, passwordInput);      // verify user password
+                VerifyUserRole(roleDb);                         // Validate role of user
+                
+                myQuery.Close();
+            }
+            else                                                    // If the row does not exists in Db
+            {
+                throw new InvalidUserException("Incorrect username or password");
+            }
+            return; // Exit function
+        }
+
+        // Validate password of the user
+        public void VerifyPassword(string passwordDb, string passwordInput)
+        {
+            if (passwordInput != passwordDb)
+            {
+                throw new InvalidUserException("Incorrect username or password");
+            }
+        }
+
+
+        // Validate role of the user
+        public void VerifyUserRole(string role)
+        {
+            // If role of user is admin, open admin panel
+            if (role == "admin")
+            {
+                AdminPanel admin = new AdminPanel();
+                admin.Show();
+            }
+
+            // If role of user is planner, open planner panel
+            else if (role == "planner")
+            {
+                planner_panel planner = new planner_panel();
+                planner.Show();
+            }
+
+            // If role of user is buyer, open planner panel
+            else if (role == "buyer")
+            {
+                buyer_panel buyer = new buyer_panel();
+                buyer.Show();
+            }
+
+            else
+            {
+                throw new InvalidUserException("Role does not exist! Please select from: admin, buyer, planner");
+            }
+        }
+    }
+}
--- Omnicorp/MainWindow.xaml.cs
+++ Omnicorp/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Data;
 using Omnicorp.Admin;
 using Omnicorp.Planner;
 using Omnicorp.Buyer;
+using Omnicorp.Exceptions;
+using ClassLibrary;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 
 namespace Omnicorp
@@ -20,108 +23,31 @@ namespace Omnicorp
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        // Accessor fields
-        string username;
-        string password;
-
-
-        public string Username
-        {
-            get { return username; }
-            set { username = value; }
-        }
-
-        public string Password
-        {
-            get { return password; }
-            set { password = value; }
+            Username_Text.Focus();
         }
 
         // Login submit button
         private void Login_Button(object sender, RoutedEventArgs e)
         {
+            LoginHandler loginHandler = new LoginHandler();
+            string usernameInput = Username_Text.Text;
+            string passwordInput = Password_Text.Text;
 
-            Username = Username_Text.Text;
-            Password = Password_Text.Text;
-
-            string connetionString = @"
-                    server=127.0.0.1;
-                    database=omnicorp;
-                    uid=root;
-                    pwd=;"; // Write server, database, root, and password into string
-            MySqlConnection connection = new MySqlConnection(connetionString);  // Establish connection between server and MySQL
-
-            // Exception handler to open connection to MySQL database
             try
             {
-                connection.Open();
+                loginHandler.TryLogin(usernameInput, passwordInput);
+                this.Close();
             }
-            catch (Exception ex)
+            catch (InvalidUserException ex)
             {
-                MessageBox.Show($"Cannot connect to database. Error: {ex}");
-                return;
+                MessageBox.Show(ex.Message, "Login Error");
             }
-
-
-            string query = $"SELECT * FROM users WHERE username = '{Username}' and password = '{Password}'; ";
-            MySqlCommand cmd = new MySqlCommand(query, connection);  // Enter command into omnicorp database
-            MySqlDataReader rdr = cmd.ExecuteReader();  // Read rows in in user table
-
-            if (rdr.Read())
-            {
-                string user = rdr.GetString(1).ToString();      // store user from users table
-                string password = rdr.GetString(2).ToString();  // store password from users table
-                string role = rdr.GetString(3).ToString();      // store role from users table
-
-                // Validate if user and password entered in textbox match query
-                VerifyUserRole(role); // Validate role of user
-                this.Close(); // Close login window
-                connection.Close();
-                return; // Exit function
-            }
-
-            // If while loop does not exit the function (return;), this indicates either the username or password was incorrect.
-            string messageBoxText = "Incorrect username or password";
-            string caption = "Login Error";
-            MessageBoxResult result;
-            result = MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None);
 
             Username_Text.Clear();
+            Username_Text.Focus();
             Password_Text.Clear();
         }
-
-
-        // Validate role of the user
-        private void VerifyUserRole(string role)
-        {
-            // If role of user is admin, open admin panel
-            if (role == "admin")
-            {
-                admin_panel admin = new admin_panel();
-                admin.Show();
-            }
-
-            // If role of user is planner, open planner panel
-            else if (role == "planner")
-            {
-                planner_panel planner = new planner_panel();
-                planner.Show();
-            }
-
-            // If role of user is buyer, open planner panel
-            else if (role == "buyer")
-            {
-                buyer_panel buyer = new buyer_panel();
-                buyer.Show();
-            }
-
-            else
-            {
-                MessageBox.Show("Role does not exist! Please select from: admin, buyer, planner");
-            }
-        }
+        
 
         private void LoginEnabled(object sender, TextChangedEventArgs e)
         {
