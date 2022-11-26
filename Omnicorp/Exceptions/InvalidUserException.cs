59448c8c8b2c3d15527fd2dbf6e98eca58a3be29
Bruno Borges Russian <bruno.russian.i@gmail.com>
2022-11-26 15:48:10 -0500

    Add invalid user exception

---
+++ Omnicorp/Exceptions/InvalidUserException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omnicorp.Exceptions
+{
+    public class InvalidUserException : Exception
+    {
+        public InvalidUserException(string message) : base(message) { }
+    }
+}
