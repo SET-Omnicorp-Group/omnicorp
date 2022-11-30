0edd52e7b320aaaa8ba3e32f536cec0cb74f1c25
Ali Anwar <a.anwar98@hotmail.com>
2022-11-30 16:56:21 -0500

    working on planner and buyer functionality, first commit for this branch

---
+++ ClassLibrary/MarketplaceQuery.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ClassLibrary
+{
+    public class MarketplaceQuery
+    {
+        public MySqlConnection conn;
+
+        public MarketplaceQuery()
+        {
+            string server = ConfigurationManager.AppSettings["server"];
+            string database = ConfigurationManager.AppSettings["database"];
+            string uid = ConfigurationManager.AppSettings["uid"];
+            string password = ConfigurationManager.AppSettings["password"];
+
+            string connString = $"server=159.89.117.198;" +
+                                $"database=cmp;" +
+                                $"uid=DevOSHT;" +
+                                $"pwd=Snodgr4ss!;";
+            conn = new MySqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                conn = null;
+                // @TODO log this to a file
+                return;
+            }
+        }
+
+        public MySqlDataReader DataReader(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, conn);  // Enter command into omnicorp database
+            MySqlDataReader rdr = cmd.ExecuteReader();  // Read rows in in user table
+            return rdr;
+        }
+
+        public string GetLogFileFromDatabase()
+        {
+            string query = "SELECT content FROM configs WHERE name = 'logFile';";
+            MySqlCommand cmd = new MySqlCommand(query, conn);  // Enter command into omnicorp database
+            MySqlDataReader rdr = cmd.ExecuteReader();  // Read rows in in user table
+            rdr.Read();
+
+            string logFile = rdr.GetString(0);
+            return logFile;
+        }
+
+        public void UpdateLogFileToDatabase(string logFile)
+        {
+            string updateQuery = $"UPDATE content SET content = {logFile} WHERE name = logFile;";
+            MySqlCommand cmd = new MySqlCommand(updateQuery, conn);  // Enter command into omnicorp database
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Close()
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ Omnicorp/Buyer/BuyerPanel.xaml.cs
@@ -0,0 +1,176 @@
+﻿using ClassLibrary;
+using Microsoft.Win32;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using MySql.Data;
+using System.Diagnostics;
+using MySqlX.XDevAPI;
+using System.Threading;
+using System.Configuration;
+using System.Collections.Generic;
+
+
+namespace Omnicorp.Buyer
+{
+    /// <summary>
+    /// Interaction logic for buyer_panel.xaml
+    /// </summary>
+    public partial class BuyerPanel : Window
+    {
+
+        // Accessor fields 
+        BuyerHandler handler;
+
+        public string Client_Name { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Quantity { get; set; }
+        public string JobType { get; set; }
+        public string VanType { get; set; }
+
+
+        public BuyerPanel()
+        {
+            InitializeComponent();
+            handler = new BuyerHandler();
+            HideDataGrids(); 
+        }
+
+
+        // Hide grids
+        private void HideDataGrids()
+        {
+            OrdersGrid.Visibility = Visibility.Hidden;
+            ClientContractGrid.Visibility = Visibility.Hidden;
+            AcceptBtn.Visibility = Visibility.Hidden;
+        }
+
+
+        // Show Radio button for orders 
+        private void ShowRadioButtons()
+        {
+            ActiveContracts.Visibility = Visibility.Visible;
+            ProcessingContracts.Visibility = Visibility.Visible;
+            CompletedContracts.Visibility = Visibility.Visible;
+            AllContracts.Visibility = Visibility.Visible;
+        }
+
+
+        // Hide Radio button for orders 
+        private void HideRadioButtons()
+        {
+            ActiveContracts.Visibility = Visibility.Hidden;
+            ProcessingContracts.Visibility = Visibility.Hidden;
+            CompletedContracts.Visibility = Visibility.Hidden;
+            AllContracts.Visibility = Visibility.Hidden;
+        }
+
+
+        // Display client contracts datagrid
+        private void ClientContractsBtn(object sender, RoutedEventArgs e)
+        {
+            HideDataGrids();
+            HideRadioButtons();
+            ClientContractGrid.Visibility = Visibility.Visible;
+            AcceptBtn.Visibility=Visibility.Visible;
+            ClientContractGrid.DataContext = handler.GetContractsFromDatabase();
+        }
+
+        
+
+        // Get value of selected rows
+        private void ClientContractGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid gd = (DataGrid)sender;
+            DataRowView rowSelected = gd.SelectedItem as DataRowView;
+
+            if (rowSelected == null)
+            {
+                Client_Name = String.Empty;
+                Origin = String.Empty;
+                Destination = String.Empty;
+                Quantity = 0;
+                JobType = "";
+                VanType = "";
+                return;
+            }
+
+            Client_Name = rowSelected[0].ToString();
+            Origin = rowSelected[1].ToString();
+            Destination = rowSelected[2].ToString();
+            Quantity = Convert.ToInt32(rowSelected[3]);
+            JobType = rowSelected[4].ToString();
+            VanType = rowSelected[5].ToString();
+        }
+
+        
+        
+        // Accept contracts button
+        private void AcceptContractsBtn(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                handler.InsertContractsToOrderDatabase(Client_Name, JobType, Quantity, VanType,
+                                                   Origin, Destination, "Processing");
+                MessageBox.Show("Client contract has been saved to orders");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot insert selected contract to the orders databases");
+            }
+        }
+
+
+
+        // Orders button
+        private void OrderBtn(object sender, RoutedEventArgs e)
+        {
+            HideDataGrids();
+            AllContracts.IsChecked = true;
+            ShowRadioButtons();
+            OrdersGrid.Visibility = Visibility.Visible;
+            OrdersGrid.DataContext = handler.GetOrdersFromDatabase();
+        }
+
+
+
+        // Order datagrid
+        private void OrdersGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+        // All contracts radio button
+        private void AllContractsChecked(object sender, RoutedEventArgs e)
+        {
+           OrdersGrid.DataContext = handler.GetOrdersFromDatabase();
+        }
+
+
+        // Active contracts radio button
+        private void ActiveContracts_Checked(object sender, RoutedEventArgs e)
+        {
+            OrdersGrid.DataContext = handler.GetActiveOrdersFromDatabase();
+        }
+
+
+        // Processing contracts radio button
+        private void ProcessingContracts_Checked(object sender, RoutedEventArgs e)
+        {
+            OrdersGrid.DataContext = handler.GetProcessingOrdersFromDatabase();
+        }
+
+        
+        // Completed contracts radio button
+        private void CompletedContracts_Checked(object sender, RoutedEventArgs e)
+        {
+            OrdersGrid.DataContext = handler.GetCompletedOrdersFromDatabase();
+        }
+    }
+}
