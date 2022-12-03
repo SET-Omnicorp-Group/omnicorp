ac585b4d6f3d6b0cae177411e33a1740db6b95c7
Bruno Borges Russian <bruno.russian.i@gmail.com>
2022-12-03 17:00:06 -0500

    Add DisplayInvoiceInfo

--- Omnicorp/Buyer/InvoiceWindow.xaml.cs
+++ Omnicorp/Buyer/InvoiceWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using MySql.Data.MySqlClient;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -19,9 +22,46 @@ namespace Omnicorp.Buyer
     /// </summary>
     public partial class InvoiceWindow : Window
     {
-        public InvoiceWindow()
+        BuyerHandler handler;
+
+        public InvoiceWindow(string orderId)
         {
             InitializeComponent();
+            handler = new BuyerHandler();
+            DisplayInvoiceInfo(orderId);
+        }
+
+        private void DisplayInvoiceInfo(string orderId)
+        {
+            string query = $"SELECT o.customer, o.origin, o.destination, o.jobType, o.vanType, o.quantity, i.amount, i.id, r.distance, r.totalHours " +
+                            $"FROM orders o " +
+                            $"INNER JOIN invoices i ON i.orderId = o.id " +
+                            $"INNER JOIN routes r ON r.orderId = o.id " +
+                            $"WHERE o.id = '{orderId}';";
+
+            MyQuery myQuery = new MyQuery();
+            MySqlDataReader rdr = myQuery.DataReader(query);
+            rdr.Read();
+
+            string customer = rdr.GetString(0);
+            string origin = rdr.GetString(1);
+            string destination = rdr.GetString(2);
+            string jobType = rdr.GetString(3);
+            string vanType = rdr.GetString(4);
+            string quantity = rdr.GetString(5);
+            decimal amount = rdr.GetDecimal(6);
+            string invoiceId = rdr.GetString(7);
+            string distance = rdr.GetString(8);
+            decimal totalHours = rdr.GetDecimal(9);
+
+            InvoiceNumberInfo.Content = invoiceId;
+            OrderIdInfo.Content = orderId;
+            ClientNameInfo.Content = customer;
+            RouteInfo.Content = $"{origin} -> {destination}";
+            JobDescriptionInfo.Content = $"{jobType} | {vanType}";
+            TotalDistanceInfo.Content = distance;
+            TotalHoursInfo.Content = String.Format("{0:0.00}", totalHours);
+            TotalInfo.Content = String.Format("{0:0.00}", amount);
         }
     }
 }
